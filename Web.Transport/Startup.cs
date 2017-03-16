72607961730dccbe0b213a6d4065ac2bff5da9f6
SEA\sbisht <sbisht@expedia.com>
2017-03-16 21:04:06 +0530

    First Commit

---
+++ Web.Transport/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Web.Transport.Startup))]
+namespace Web.Transport
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
